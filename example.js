10e98ba9d2f2655ff050a8b443f6ed317ce66428
Matt Travi <programmer@travi.org>
2022-06-27 20:18:15 -0400

    feat(dual-mode): defined an exports map to make this package a true dual-mode package
    BREAKING CHANGE: and exports map has been defined, which further limits access to files within the
    package

--- example.js
+++ example.js
@@ -3,7 +3,7 @@
 import {resolve} from 'path';
 import stubbedFs from 'mock-fs';
 import {dialects, projectTypes} from '@form8ion/javascript-core';
-import {scaffold} from './lib/index.cjs';
+import {scaffold} from './lib/index.js';
 
 // remark-usage-ignore-next
 stubbedFs({templates: stubbedFs.load(resolve(...[__dirname, 'templates']))});
